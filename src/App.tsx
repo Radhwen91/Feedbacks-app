264b85e481dc7e376d2771c0fe5d45947caeead9
Radhwen91 <kacemradhwen@gmail.com>
2024-05-17 15:38:50 +0100

    aa

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import Image from './components/Image.tsx';
 import Feedback from './models/Feedback.tsx';
 function App() {
   const feedbacks: Feedback[] = [
-    new Feedback('src/assets/ph4.jpg', 5, 'The coissants are perfectly flaky and buttery, providing a mouthwatery, providing a mouthwatering base for a variety of creative fillings.','w-1/2 flex flex-col items-start p-4 transform scale-110 -rotate-6','w-1/4'),
+    new Feedback('src/assets/ph1.jpg', 5, 'The coissants are perfectly flaky and buttery, providing a mouthwatery, providing a mouthwatering base for a variety of creative fillings.','w-1/2 flex flex-col items-start p-4 transform scale-110 -rotate-6','w-1/4'),
     new Feedback('src/assets/ph2.webp', 4, 'The inviting ambiance and friendly service make this restaurant a fantastic spot.','w-1/2 flex flex-col items-start p-4 transform scale-110 rotate-1','w-1/6'),
       new Feedback('src/assets/ph3.jpg', 3, 'The attention to detail and fresh ingredients make this place stand out. It\'s a must-visit for anyone.','w-1/2 flex flex-col items-start p-4 transform scale-110 -rotate-6','w-1/4'),
   ];
